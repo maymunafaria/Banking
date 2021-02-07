541782c5979da419c3028a36fb871a4c4e4f3d71
maymunafaria <76717643+maymunafaria@users.noreply.github.com>
2021-02-07 22:31:47 +0600

    Update bank.java

--- bank.java
+++ bank.java
@@ -3,11 +3,11 @@ package banking;
 public class bank {
     public void start_banking()
     {
-        Account Faria = new Account("faria" , "01778314306" , 1000, "1234");
+        Account Faria = new Account("faria" , "01789024306" , 1000, "1234");
         Faria.information();
         Faria.addmoney(1000);
         Faria.cashout(500 , "1234");
-Account Maymuna = new Account("Maymuna" , "01778314506" , 500);
+Account Maymuna = new Account("Maymuna" , "01394714506" , 500);
 Maymuna.information();
 Maymuna.addmoney(1000);
 Maymuna.pin("4321");
